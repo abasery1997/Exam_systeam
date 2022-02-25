2be25c4b8c22acfcc38d56d9b908b62edf51e671
fadwaMohamed <98125155+fadwaMohamed@users.noreply.github.com>
2022-02-25 14:48:13 +0200

    mssql

---
+++ testQuery.js
@@ -0,0 +1,41 @@
+const config = require('./dbconfig')
+const sql = require('mssql')
+
+
+async function getSt() {
+    try {
+        let value = 5
+        let pool = await sql.connect(config);
+        let result1 = await pool.request().query('select * from students')
+            
+        // stored procedure
+        let result2 = await pool.request()
+            .input('courseID', sql.Int, value)
+            //.output('output_parameter', sql.VarChar(50))
+            .execute('getCourseInfo')
+
+        //console.dir(result1.recordset);
+        return result2.recordset;
+
+    } catch (err) {
+        // ... error checks
+        return new Error("failed to connect")
+    }
+}
+
+getSt().then(result => {
+    console.log(result)
+    console.log("-----------------------------------------------")
+    for(let i in result)
+    {
+        for(let v in result[i])
+        {
+            console.log(`${v}: ${result[i][v]}`);
+        }
+    }
+})
+
+sql.on('error', err => {
+    // ... error handler
+    console.log(err)
+})
