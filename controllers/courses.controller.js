b2b108bb7674ad844677e6fc2175bb06a91e23be
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-02-25 22:24:48 +0200

    new Course with  2 topics

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -2,11 +2,32 @@ const sql = require('mssql')
 
 
 
-
+//get all courses
 exports.list = function (req, res, next) {
     new sql.Request().execute('SelectAllCourse')
         .then(result => {
             res.status(200).json({ message: "Courses data", data: result.recordset });
+        })
+        .catch(error => {
+            error.status = 500;
+            next(error);
+        })
+}
+
+//add a course and two topics
+
+exports.addCourse = function (req, res, next) {
+
+    //course data
+    new sql.Request()
+        .input('Name', sql.NVarChar, req.body.Name)
+        .input('Insructor_id', sql.Int, req.body.InsId)
+        .input('FirstTopName', sql.NVarChar, req.body.fTopicName)
+        .input('SecondTopName', sql.NVarChar, req.body.sTopicName)
+        .execute('CreateCourse')
+        .then(result => {
+             res.status(200).json({ message:"course Added successfully" });
+
         })
         .catch(error => {
             error.status = 500;
--- routes/courses.router.js
+++ routes/courses.router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {list} = require("../controllers/courses.controller");
+const {list,addCourse} = require("../controllers/courses.controller");
 
 
 router.get('/list', list);
+router.post('/add', addCourse);
 
 module.exports = router;
 
