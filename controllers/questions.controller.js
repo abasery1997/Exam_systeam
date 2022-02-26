2d65f21eb05475e2772e364a3bc2486d7f3ee84c
Abd El-Rahman Abd El-Zaher <abdoumustafa1997@gmail.com>
2022-02-26 23:49:12 +0200

    get  courseQuestions

--- controllers/questions.controller.js
+++ controllers/questions.controller.js
@@ -8,29 +8,65 @@ exports.list = function (req, res, next) {
         .then(result => {
             let questions = result.recordset;
             questions.forEach((Quest, index) => {
-               
-                    new sql.Request()
-                        .input('ansId', sql.Int, Quest.Correct_Answer)
-                        .execute('GetCorrectAnswer')
-                        .then(result => {
-                            choise = result.recordset[0].Body;
-                            Quest.Correct_Answer = choise
-                            
-                            Quest= Object.assign(Quest, {'Choices': []});
-                            new sql.Request()
+
+                new sql.Request()
+                    .input('ansId', sql.Int, Quest.Correct_Answer)
+                    .execute('GetCorrectAnswer')
+                    .then(result => {
+                        choise = result.recordset[0].Body;
+                        Quest.Correct_Answer = choise
+
+                        Quest = Object.assign(Quest, { 'Choices': [] });
+                        new sql.Request()
                             .input('questionID', sql.Int, Quest.Ques_ID)
                             .execute('getQuestionChoices')
                             .then(result => {
                                 result.recordset.forEach(Choice => {
-                                    Quest.Choices.push(Choice.Body); 
+                                    Quest.Choices.push(Choice.Body);
                                 })
-                                if(index == questions.length-1)
-                                res.status(200).json({ message: "Questions data", questions });
+                                if (index == questions.length - 1)
+                                    res.status(200).json({ message: "Questions data", questions });
                             })
-                           
-                        })
+
+                    })
             })
-           
+
+        })
+        .catch(error => {
+            error.status = 500;
+            next(error);
+        })
+}
+exports.courseQuestions = function (req, res, next) {
+    new sql.Request()
+        .input('courseID', sql.Int, req.body.CrsId)
+        .execute('getCourseQuestions')
+        .then(result => {
+            let questions = result.recordset;
+            questions.forEach((Quest, index) => {
+
+                new sql.Request()
+                    .input('ansId', sql.Int, Quest.Correct_Answer)
+                    .execute('GetCorrectAnswer')
+                    .then(result => {
+                        choise = result.recordset[0].Body;
+                        Quest.Correct_Answer = choise
+
+                        Quest = Object.assign(Quest, { 'Choices': [] });
+                        new sql.Request()
+                            .input('questionID', sql.Int, Quest.Ques_ID)
+                            .execute('getQuestionChoices')
+                            .then(result => {
+                                result.recordset.forEach(Choice => {
+                                    Quest.Choices.push(Choice.Body);
+                                })
+                                if (index == questions.length - 1)
+                                    res.status(200).json({ message: "Questions data", questions });
+                            })
+
+                    })
+            })
+
         })
         .catch(error => {
             error.status = 500;
--- routes/questions.router.js
+++ routes/questions.router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {list} = require("../controllers/questions.controller");
+const {list,courseQuestions} = require("../controllers/questions.controller");
 
 
 router.get('/list', list);
+router.get('/courseQuestions', courseQuestions);
 
 module.exports = router;
